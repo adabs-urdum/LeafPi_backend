5642c9d3cbc10dc2563e3b06ee4eb0e0e0ccbc88
adabs-urdum <cyrill.lehmann@clus.ch>
2019-03-24 00:17:11 +0100

    home view

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,25 +8,8 @@ use Symfony\Component\Process\Process;
 use Symfony\Component\Process\Exception\ProcessFailedException;
 
 class Homecontroller extends Controller{
-  public function home()
-  {
-    $env = env('APP_ENV');
-    if($env == "local_casterly_rock"):
-    endif;
-    $command = "python3 ./python_scripts/turn_off.py";
-    $cwd = null;
-    $envVars = [ 'HOME' => getEnv('HOME'), 'PATH' => getEnv('PATH') ];
-    $process = new Process($command, $cwd, $envVars);
-    $process->run();
-
-    // executes after the command finishes
-    if (!$process->isSuccessful()) {
-      throw new ProcessFailedException($process);
-    }
-
-    echo $process->getOutput();
-    // Result (string): {'neg': 0.204, 'neu': 0.531, 'pos': 0.265, 'compound': 0.1779}
-      return view('welcome');
+  public function home(){
+    return view('welcome');
   }
 
   public function turnOff(){
